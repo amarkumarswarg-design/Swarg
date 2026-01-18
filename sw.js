a652ea99b75359c38d6b36f904347e2f416e7144
amarkumarswarg-design <amarkumarswarg@gmail.com>
2026-01-18 12:08:06 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,13 @@
 self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open('swarg-store').then((cache) => cache.addAll([
+    caches.open('swarg-cache').then((cache) => cache.addAll([
       'index.html',
-      'manifest.json',
+      'manifest.json'
     ])),
   );
 });
 
 self.addEventListener('fetch', (e) => {
-  console.log(e.request.url);
   e.respondWith(
     caches.match(e.request).then((response) => response || fetch(e.request)),
   );
